ee175f592910bbcc53731cdb0a40cd1f417df937
yrmint <zhilkinal@yandex.ru>
2023-03-23 19:39:54 +0300

    module7 task1-3

--- js/data.js
+++ js/data.js
@@ -2,20 +2,20 @@ import { getRandom } from './util.js';
 
 // генерация фоторгафии
 const generatePhoto = (number) => ({
-    id: number,
-    url: `photos/${ number+1 }.jpg`,
-    description: `look at my cat!!`,
-    likes: getRandom(15, 200),
-    comments: getRandom(0, 200),
+  id: number,
+  url: `photos/${ number+1 }.jpg`,
+  description: `look at my cat!!`,
+  likes: getRandom(15, 200),
+  comments: getRandom(0, 200),
   });
   
-  // массив с фотографиями
-  let photos = [];
+// массив с фотографиями
+let photos = [];
   
-  // генерация массива фотографий
-  export const generatePhotos = (number) => {
-    for(let i = 0; i < number; i++) {
-      photos[i] = generatePhoto(i);
-    }
-    return photos;
-  };
+// генерация массива фотографий
+export const generatePhotos = (number) => {
+  for(let i = 0; i < number; i++) {
+    photos[i] = generatePhoto(i);
+  }
+  return photos;
+};

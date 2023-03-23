f01ea8da699892e32142266b5fe9e332cf554ca4
yrmint <zhilkinal@yandex.ru>
2023-03-23 19:49:54 +0300

    module7 task1-4

--- js/photoMiniature.js
+++ js/photoMiniature.js
@@ -2,14 +2,14 @@ const pictureTemplate = document.querySelector('#picture');
 const pictureContainer = document.querySelector('.pictures');
 
 export function insertPhotoMiniature(photos) {
-    const pictureFragment = document.createDocumentFragment();
-    for (const {url, likes, comments} of photos) {
-        const picture = pictureTemplate.cloneNode(true).content;
-        picture.querySelector('.picture__img').src = url;
-        picture.querySelector('.picture__likes').textContent = likes;
-        picture.querySelector('.picture__comments').textContent = comments.length;
-        pictureFragment.appendChild(picture);
-    }
+  const pictureFragment = document.createDocumentFragment();
+  for (const {url, likes, comments} of photos) {
+      const picture = pictureTemplate.cloneNode(true).content;
+      picture.querySelector('.picture__img').src = url;
+      picture.querySelector('.picture__likes').textContent = likes;
+      picture.querySelector('.picture__comments').textContent = comments.length;
+      pictureFragment.appendChild(picture);
+  }
 
-    pictureContainer.appendChild(pictureFragment);
+  pictureContainer.appendChild(pictureFragment);
 }

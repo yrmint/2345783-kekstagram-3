c94a9a5d360fc45cf990ffcfa95297d4b88f81b9
yrmint <zhilkinal@yandex.ru>
2023-04-04 19:49:01 +0300

    8-1

--- js/form.js
+++ js/form.js
@@ -6,28 +6,28 @@ const hashtag = document.querySelector('.text__hashtags');
 const comment = document.querySelector('.text__description');
 
 function cleanForm() {
-    downloadButton.value = '';
-    hashtag.value = '';
-    comment.value = '';
-}
-
-function openDownloadWindow() {
-    document.querySelector('.img-upload__overlay').classList.remove('hidden');
-    document.body.addEventListener('keydown', onFormEscapeKeyDown);
-}
-
-function closeDownloadWindow() {
-    document.querySelector('.img-upload__overlay');
-    document.body.classList.remove('modal-open');
-    document.removeEventListener('keydown', onFormEscapeKeyDown);
-    cleanForm();
+  downloadButton.value = '';
+  hashtag.value = '';
+  comment.value = '';
 }
 
 const onFormEscapeKeyDown = (e) => {
-    if (isEscapeKey(e)) {
-        e.preventDefault();
-        closeDownloadWindow();
-    }
+  if (isEscapeKey(e)) {
+      e.preventDefault();
+      closeDownloadWindow();
+  }
+}
+  
+function openDownloadWindow() {
+  document.querySelector('.img-upload__overlay').classList.remove('hidden');
+  document.body.addEventListener('keydown', onFormEscapeKeyDown);
+}
+
+function closeDownloadWindow() {
+  document.querySelector('.img-upload__overlay');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onFormEscapeKeyDown);
+  cleanForm();
 }
 
 downloadButton.addEventListener('change', () => { openDownloadWindow(); });

3a3a7916d6a881c8156d823e090d2631160995e1
yrmint <zhilkinal@yandex.ru>
2023-04-14 00:57:03 +0300

    4

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ const BACKEND_URL = 'https://27.javascript.pages.academy/kekstagram-simple/data'
 
 export function getPhotos (onSuccess, onError) {
   fetch(BACKEND_URL)
-  .then((response) => response.json())
-  .then(onSuccess)
-  .catch(onError);
+    .then((response) => response.json())
+    .then(onSuccess)
+    .catch(onError);
 }
--- js/form.js
+++ js/form.js
@@ -22,7 +22,7 @@ function cleanForm() {
   imgElement.classList.remove(oldEffect);
   imgElement.classList.add('effects__preview--none');
   scaleValueElement.value = '100%';
-  imgElement.style = 'transform: scale(${parseInt(scaleValueElement.value, 10) / 100})';
+  imgElement.style = `transform: scale(${parseInt(scaleValueElement.value, 10) / 100})`;
 
 }
 
